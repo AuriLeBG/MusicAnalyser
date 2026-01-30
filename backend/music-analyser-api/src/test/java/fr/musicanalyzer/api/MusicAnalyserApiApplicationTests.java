310e7dd6abc876f5c0da70d79b7a3f3bd97120bf
valdev <valentin.devisme@hydrelis.com>
2026-01-30 14:55:44 +0100

    [20260130] Init Backend repository

---
+++ backend/music-analyser-api/src/test/java/fr/musicanalyzer/api/MusicAnalyserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.musicanalyzer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAnalyserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
